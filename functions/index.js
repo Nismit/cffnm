db0ed479918e28fd65ac45e2f96380f331bb4b07
Nismit <mitch@engagemassive.com>
2020-06-16 17:37:03 -0700

    :sparkles: feat: Send Email Function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const CORS = require('cors')({ origin: true });
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 465,
+    secure: true,
+    auth: {
+        user: functions.config().someservice.email,     // 'yourgmailaccount@gmail.com'
+        pass: functions.config().someservice.password   // 'yourgmailaccpassword'
+    }
+});
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+    CORS(req, res, () => {
+        const dest = req.query.dest;
+
+        const mailOptions = {
+            from: 'John Doe <example@example.com>',
+            to: dest,
+            subject: 'Test Mail', // email subject
+            text: 'Plain Text Test Email',
+            html: '' // HTML Mail
+        };
+
+        return transporter.sendMail(mailOptions, (error, info) => {
+            if(error) {
+                return res.send(error.toString());
+            }
+
+            return res.send('Mail Sended');
+        });
+    })
+});
